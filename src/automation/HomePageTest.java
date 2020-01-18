b4d8ab7504910f349277fa598bf09a203de9a2c5
li0rojo <rafael_11li@hotmail.com>
2020-01-17 18:00:24 -0600

    Add files via upload
    Last update January 17, 2020

---
+++ src/automation/HomePageTest.java
@@ -0,0 +1,43 @@
+package automation;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test; 
+import org.openqa.selenium.By; 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+class HomePageTest {
+
+	 @BeforeEach
+	   void setUp() {
+	   }
+
+	   @BeforeEach
+	   void tearDown() {
+	   }
+	
+	@Test
+    void test() {
+		fail("Not yet implemented");
+	}
+	
+	
+	
+	public void site_header_is_on_home_page() {   
+	       System.setProperty("webdriver.chrome.driver","c\\users\\matt\\chromedriver.exe");   
+	       WebDriver browser = new ChromeDriver();
+	       browser.get("https://www.saucelabs.com");       
+	       WebElement href = browser.findElement(By.xpath("//a[@href='/beta/login']"));
+
+}
+    assertTrue((href.isDisplayed()));  
+browser.close();
+
+}
